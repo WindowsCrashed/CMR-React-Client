13569e7b0efbba2a61a71b3b51f78668e1d18ac8
WindowsCrashed <windowscrashed0000@gmail.com>
2022-12-04 14:54:19 -0300

    Sending to remote

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react'
 import './global.css'
 
 function App() {
   return (
-    <h1 className='brand'>Hello World!</h1>
+    <React.Fragment>
+      <h1 className='brand'>Hello World!</h1>
+      <h2>Testing</h2>
+    </React.Fragment>
   )
 }
 
